b9c3a50e6291f800f2fe1d76e869f6f719b65327
Younes <sariouiyouness@gmail.com>
2026-04-24 11:23:24 +0100

    Correction erreur build: export default sur checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,8 @@
+export default function CheckoutPage() {
+  return (
+    <div style={{ padding: '100px', textAlign: 'center' }}>
+      <h1>Page de Paiement</h1>
+      <p>Bientôt disponible.</p>
+    </div>
+  )
+}
